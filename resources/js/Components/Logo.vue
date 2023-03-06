f5c8eac346f85b4bf32825fa79a62493ba9a8fc3
Yves Gonzaga <yves2regun@gmail.com>
2023-03-06 21:34:12 +0800

    Added Logo and ProfilePicture component

---
+++ resources/js/Components/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="logo" class="flex items-center justify-center">
+        <img src="https://canzell.com/wp-content/uploads/2022/01/CR-Yardsign-Logo.png">
+    </div>
+</template>
+<style>
+#logo {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    position: relative;
+    padding: 1rem 3rem 6rem 3rem;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+}
+</style>
---
+++ resources/js/Components/ProfilePicture.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="profile" class="flex items-center justify-center">
+        <img class="rounded-full" :src="user.mug">
+    </div>
+</template>
+<script>
+export default {
+    props: ['user'],
+}
+</script>
+<style>
+#profile img {
+    width: 7rem;
+    height: 7rem;
+    border-radius: 100%;
+    text-align: center;
+    pointer-events: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    margin-top: -4.5rem;
+    width: 10rem;
+    height: 10rem;
+    border-radius: 100%;
+    box-sizing: content-box;
+    pointer-events: none;
+    user-select: none;
+    margin-top: -6rem;
+}
+</style>
