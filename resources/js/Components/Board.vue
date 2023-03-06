cb070046e7ba20bbed2f41641d2e8ed3e08b8993
Yves Gonzaga <yves2regun@gmail.com>
2023-03-06 21:33:14 +0800

    Added Board and Card layout component

---
+++ resources/js/Components/Board.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Logo from './Logo.vue';
+</script>
+<template>
+    <div :id="id" class="board" @dragover.prevent @drop.prevent="drop">
+        <Logo></Logo>
+        <slot />
+    </div>
+</template>
+<script>
+</script>
+<style>
+.board {
+    background-color: #840202;
+    padding: 20px;
+    color: black;
+    width: 500px;
+    margin: 20px auto;
+}
+</style>
---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ProfilePicture from './ProfilePicture.vue';
+</script>
+<template>
+    <div :id="id" class="card" :test="folder" :draggable="draggable" @dragstart="dragstart" @dragover.stop>
+        <ProfilePicture :user="user"></ProfilePicture>
+        <slot />
+    </div>
+</template>
+<script>
+export default {
+    props: ['user'],
+}
+</script>
+<style>
+.card {
+    background: rgb(243, 243, 243);
+    margin: 10px;
+    padding-bottom: 2rem;
+    border-radius: 20px;
+}
+</style>
