ce3fbda20d6ab1f1b1876d6d24a8c5f2cdeb2b8f
Yves Gonzaga <yves2regun@gmail.com>
2023-03-06 21:32:11 +0800

    Added Users and User Pages

---
+++ resources/js/Pages/User.vue
@@ -0,0 +1,36 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Board from '../Components/Board.vue';
+import Card from '../Components/Card.vue';
+import Action from '../Components/action/Index.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <notifications />
+
+    <Head title="User View" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">User View</h2>
+        </template>
+
+        <div style="background-color: #840202;" class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex items-center justify-center">
+                    <Board>
+                        <Card :user="user">
+                            <Action :user="user"></Action>
+                        </Card>
+                    </Board>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+<script>
+export default {
+    props: ['user'],
+}
+</script>
---
+++ resources/js/Pages/Users.vue
@@ -0,0 +1,127 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Logo from '../Components/Logo.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+<template>
+    <notifications />
+
+    <Head title="Users" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Users</h2>
+        </template>
+
+        <div style="background-color: #840202;" class="py-12">
+
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <Logo></Logo>
+                <h1>List of Agents</h1>
+                <div class="flex items-center justify-center">
+
+                    <div class="users">
+                        <div v-for="user in users" class="user">
+                            <div class="info">
+                                <a target="_self" rel="noopener noreferrer" aria-label="website" :href="'users/' + user.id"
+                                    style="background-color: rgb(14, 14, 14);">
+                                    <img :src="user.mug">
+                                    <p class="text">
+                                        {{ user.first_name + " " + user.last_name }}
+                                    </p>
+                                </a>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+<script>
+export default {
+    props: ['users'],
+    mounted() {
+        console.log()
+    },
+    methods: {
+
+    }
+}
+</script>
+<style>
+.st0Unique {
+    fill: #fff !important
+}
+
+.users {
+    margin: 2rem 0;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+}
+
+.user {
+    padding: 0.5rem;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+}
+
+.user {
+    width: 24.33%;
+}
+
+.user img {
+    width: 7rem;
+    height: 7rem;
+    border-radius: 100%;
+    text-align: center;
+    pointer-events: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    width: 10rem;
+    height: 10rem;
+    border-radius: 100%;
+    box-sizing: content-box;
+    pointer-events: none;
+    user-select: none;
+}
+
+.info p {
+    margin: 0.5rem 0 0;
+    font-size: 0.875rem;
+}
+
+.text {
+    text-align: center;
+    line-height: 1.5;
+    color: #ffffff !important;
+}
+h1 {
+    text-align: center;
+    font-size: 3rem;
+    font-weight: bold;
+    text-transform: uppercase;
+    color: white;
+}
+</style>
